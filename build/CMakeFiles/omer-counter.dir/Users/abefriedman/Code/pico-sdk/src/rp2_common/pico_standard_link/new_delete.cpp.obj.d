c15459fa8b044bb88c2a4030e2b38d0065494027
rabbiabe <asfriedman@gmail.com>
2024-04-10 21:04:49 -0400

    update .gitignore

--- build/CMakeFiles/omer-counter.dir/Users/abefriedman/Code/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/omer-counter.dir/Users/abefriedman/Code/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp.obj: \
- /Users/abefriedman/Code/pico-sdk/src/rp2_common/pico_standard_link/new_delete.cpp
